1bf343076ff6c2a1e797891ac367113f33139d92
i-martello <martelloignacio@gmail.com>
2023-02-17 04:01:26 -0300

    cambios

--- src/server/config.js
+++ src/server/config.js
@@ -20,7 +20,7 @@ module.exports = app =>{
   app.set('view engine', '.hbs')
   //middlewares
   app.use(morgan('dev'))
-  app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'))
+  app.use(multer({ dest: "./upload" }).single("image"));
   app.use(express.urlencoded({extended:false}))
   app.use(express.json())
   //routes
