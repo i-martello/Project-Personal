0682f1582f41bd8d3823f2841ec87c28f641d86b
i-martello <martelloignacio@gmail.com>
2023-04-03 18:27:40 -0300

    cambios

---
+++ src/controllers/upload.js
@@ -0,0 +1,7 @@
+const ctrl = {}
+
+ctrl.index = (req, res) => {
+  res.render('partials/upload');
+}
+
+module.exports = ctrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,11 +2,13 @@ const {Router} = require('express')
 const router = Router()
 const home = require('../controllers/home')
 const image = require('../controllers/image')
+const upload = require('../controllers/upload')
 
 module.exports = app =>{
   app.use(router)
   router.get('/', home.index )
   router.get('/images/:image_id', image.index )
+  router.get('/publicar', upload.index)
   router.post('/images', image.create ) 
   router.post('/images/:image_id/like', image.like )
   router.post('/images/:image_id/comment', image.comment )
